e613b343f34063cec7202cf7f7ee45421fdc023d
Mats Neander <matsneander@gmail.com>
2012-11-11 11:14:13 +0100

    Added drag and drop positioning

--- InputfieldMapMarker.js
+++ InputfieldMapMarker.js
@@ -6,28 +6,41 @@
 var InputfieldMapMarker = {
 
 	options: {
-		zoom: 5, 
+		zoom: 12, 
+		draggable: true,
 		center: null,
 		mapTypeId: google.maps.MapTypeId.HYBRID,
 		scrollwheel: false,	
 		mapTypeControlOptions: {
 			style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
 		},
-		scaleControl: false
+		scaleControl: false,
+		
 	},	
 
 	init: function(mapId, lat, lng) {
 		var options = InputfieldMapMarker.options; 
 		options.center = new google.maps.LatLng(lat, lng); 	
-		options.zoom = 5; 
+		options.zoom = 12; 
 		var map = new google.maps.Map(document.getElementById(mapId), options); 	
 		var marker = new google.maps.Marker({
 			position: options.center, 
-			map: map
+			map: map,
+			draggable: options.draggable
 		}); 
+
+ 		document.getElementById("_Inputfield_karta_lat").value = marker.getPosition().lat();
+        document.getElementById("_Inputfield_karta_lng").value = marker.getPosition().lng();
+
+        google.maps.event.addListener(marker, 'dragend', function (event) {
+            document.getElementById("_Inputfield_karta_lat").value = this.getPosition().lat();
+            document.getElementById("_Inputfield_karta_lng").value = this.getPosition().lng();
+        });
 		
 	}
 };
+
+
 
 $(document).ready(function() {
 	$(".InputfieldMapMarkerMap").each(function() {
@@ -35,3 +48,4 @@ $(document).ready(function() {
 		InputfieldMapMarker.init($t.attr('id'), $t.attr('data-lat'), $t.attr('data-lng')); 
 	}); 
 }); 
+
